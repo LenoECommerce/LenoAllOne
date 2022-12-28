c4c58fb8f7a6b95bebaee0dab05be0e93579d3d9
DESKTOP-A1SF6DF\lenna <lennartaufermann@gmail.com>
2022-12-28 10:22:51 +0100

    tried to change name of application

--- EigenbelegToolAlpha/Properties/AssemblyInfo.cs
+++ EigenbelegToolAlpha/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("EigenbelegToolAlpha")]
+[assembly: AssemblyTitle("Leno Software")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EigenbelegToolAlpha")]
+[assembly: AssemblyProduct("Leno Software")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
